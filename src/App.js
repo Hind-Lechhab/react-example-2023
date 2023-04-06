bfde491ef175647d2b4538a68d879cd11baaec9d
MdAmine <saoud93@gmail.com>
2023-04-06 15:29:03 +0000

    Add Routing + Fetch API + Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import ControlledForm from "./examples/5-form/ControlledForm";
 import UncontrolledFormAndUseRef from "./examples/5-form/UncontrolledFormAndUseRef";
+import RoutingExample from "./examples/6-routing/RoutingExample";
+import FetchApiExample from "./examples/7-fetchApi/FetchApiExample";
+import ContextExample from "./examples/8-contextApi/ContextExample";
 
 function App() {
   return (
     <>
-      <ControlledForm />
+      <ContextExample />
     </>
   );
 }
---
+++ src/examples/6-routing/Page3.js
@@ -0,0 +1,19 @@
+import { useNavigate, useParams } from "react-router-dom";
+
+const Page3 = () => {
+  const { id } = useParams();
+  let navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Page 3</h1>
+      <h3>ID : {id}</h3>
+
+      <button onClick={() => navigate(-1)}>Back</button>
+      <button onClick={() => navigate(1)}>Next</button>
+
+    </div>
+  );
+};
+
+export default Page3;
